14973cc14fe6aab009e3061fc718aab853269bbc
aravind-enduri781 <enduriaravind2001@gmail.com>
2025-07-31 00:48:53 +0530

    Completed basic workflow engine with step execution

--- src/main/java/com/workflow/workflow_engine/WorkflowEngineApplication.java
+++ src/main/java/com/workflow/workflow_engine/WorkflowEngineApplication.java
@@ -1,13 +1,35 @@
 package com.workflow.workflow_engine;
 
+import com.workflow.workflow_engine.model.StepType;
+import com.workflow.workflow_engine.model.WorkflowStep;
+import com.workflow.workflow_engine.core.WorkflowEngine;
+import  com.workflow.workflow_engine.model.Workflow;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.Arrays;
+
+
 public class WorkflowEngineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkflowEngineApplication.class, args);
+		//Create workflow steps
+
+		WorkflowStep step1 = new WorkflowStep("1", "Validate Resquest", StepType.AUTOMATED, "2");
+		// Step 2: Manual step
+		WorkflowStep step2 = new WorkflowStep("2", "Manager Approval", StepType.MANUAL, "3");
+
+// Step 3: External step
+		WorkflowStep step3 = new WorkflowStep("3", "Notify External System", StepType.EXTERNAL, null);
+
+
+		//create the workflow
+		Workflow workflow = new Workflow("WF001","Onboarding Workflow", Arrays.asList(step1, step2, step3));
+		//Execute workflow
+		WorkflowEngine engine = new WorkflowEngine();
+		engine.execute(workflow);
 	}
 
 }
--- src/main/java/com/workflow/workflow_engine/model/WorkflowStep.java
+++ src/main/java/com/workflow/workflow_engine/model/WorkflowStep.java
@@ -5,13 +5,13 @@ import java.util.Objects;
 public class WorkflowStep {
         private String id;
         private String name;
-        private StepType type; // ENUM: MANUAL, AUTO, EXTERNAL
+        private StepType stepType; // ENUM: MANUAL, AUTO, EXTERNAL
         private String nextStepId;
 
-        public WorkflowStep(String id, String name, StepType type, String nextStepId) {
+        public WorkflowStep(String id, String name, StepType stepType, String nextStepId) {
             this.id = id;
             this.name = name;
-            this.type = type;
+            this.stepType = stepType;
             this.nextStepId = nextStepId;
         }
 
@@ -32,11 +32,11 @@ public class WorkflowStep {
         }
 
         public StepType getType() {
-            return type;
+            return stepType;
         }
 
         public void setType(StepType type) {
-            this.type = type;
+            this.stepType = type;
         }
 
         public String getNextStepId() {
@@ -52,12 +52,12 @@ public class WorkflowStep {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             WorkflowStep that = (WorkflowStep) o;
-            return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(type, that.type) && Objects.equals(nextStepId, that.nextStepId);
+            return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(stepType, that.stepType) && Objects.equals(nextStepId, that.nextStepId);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(id, name, type, nextStepId);
+            return Objects.hash(id, name, stepType, nextStepId);
         }
 
         @Override
@@ -65,7 +65,7 @@ public class WorkflowStep {
             return "WorkflowStep{" +
                     "id='" + id + '\'' +
                     ", name='" + name + '\'' +
-                    ", type=" + type +
+                    ", type=" + stepType +
                     ", nextStepId='" + nextStepId + '\'' +
                     '}';
         }
